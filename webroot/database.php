75603145a2da2273edec5cc44faa5a6f39255d4c
imaikev <47906773+imaikev@users.noreply.github.com>
2019-03-29 12:52:08 -0300

    Update database.php

--- webroot/database.php
+++ webroot/database.php
@@ -2,7 +2,7 @@
 class Database
 {
     private static $dbName = 'prd' ;  
-    private static $dbHost = $_SERVER['MYSQL_SERVICE_HOST'];
+    private  $dbHost = $_SERVER['MYSQL_SERVICE_HOST'];
     private static $dbUsername = 'prd';
     private static $dbUserPassword = 'prd';
      
